95d622ba18601269d92a9bbcec019e249d680d2c
Douglas Godois dos Santos <doug_potter92@hotmail.com>
2022-12-04 12:55:31 -0300

    atualizando dados

--- js/mains.js
+++ js/mains.js
@@ -4,18 +4,18 @@ const updateProfileInfo = (profileData) => {
     photo.alt = profileData.name;
 
 
-    const name = document.getElementById('profile.name');
+    const name = document.getElementById('profile.name')
     name.innerText = profileData.name;
-    const job = document.getElementById('profile.job');
+    const job = document.getElementById('profile.job')
     job.innerText = profileData.job;
     const location = document.getElementById('profile.location');
     location.innerText = profileData.location;
-    const phone = document.getElementById('profile.phone');
+    const phone = document.getElementById('profile.phone')
     phone.innerText = profileData.phone;
-    phone.href = `tel:${profileData.phone}`;
-    const email = document.getElementById('profile.email');
+    phone.href = `tel: ${profileData.phone}`
+    const email = document.getElementById('profile.email')
     email.innerText = profileData.email;
-    email.href = `mailto:${profileData.email}`;
+    email.href = `mailto:${profileData.email}`
 
 
 };
