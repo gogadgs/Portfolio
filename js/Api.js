8d1edc2033816695d8438689d7cc60a7d63510a2
Douglas Godois dos Santos <doug_potter92@hotmail.com>
2023-01-03 12:05:26 -0300

    json

--- js/Api.js
+++ js/Api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData  (){
-    const url = "/json/profile.json"; // arquivo json a ser inserido e construido // 
+    const url = "https://raw.githubusercontent.com/gogadgs/Portfolio/main/json/profile.json"; // arquivo json a ser inserido e construido // 
      const fetching =  await fetch(url);
     return await  fetching.json();
 };
